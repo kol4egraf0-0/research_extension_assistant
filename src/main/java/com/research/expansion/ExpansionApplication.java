40b44f932a326315628c542df68099003fb9a575
kol4egraf0-0 <genitya408@gmail.com>
2025-04-29 15:41:57 +0300

    initial initial

---
+++ src/main/java/com/research/expansion/ExpansionApplication.java
@@ -0,0 +1,13 @@
+package com.research.expansion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpansionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpansionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/research/expansion/ExpansionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.research.expansion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpansionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
